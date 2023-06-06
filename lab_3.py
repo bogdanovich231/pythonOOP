8710fb04a6d9949713b2685b5a09ef9db52b7dd9
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-06 17:53:12 +0200

    feat: creating class Rectangle| perimeter and footage methods

--- lab_3.py
+++ lab_3.py
@@ -22,6 +22,17 @@ class Square(Figure):
     def calculate_perimeter(self):
         return 4 * self.side
 
+class Rectangle(Figure):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.length + self.width)
+
 choice = int(input("Choose a figure:\n1 - Square\n2 - Rectangle\n3 - Triangle\n4 - Circle\n5 - Rhombus\n6 - Trapeze\n"))
 
 if choice == 1:
@@ -29,3 +40,10 @@ if choice == 1:
     square = Square(side)
     print("Square footage:", square.calculate_area())
     print("The perimeter of the square:", square.calculate_perimeter())
+
+elif choice == 2:
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+    rectangle = Rectangle(length, width)
+    print("Rectangle footage:", rectangle.calculate_area())
+    print("Perimeter of the rectangle:", rectangle.calculate_perimeter())
