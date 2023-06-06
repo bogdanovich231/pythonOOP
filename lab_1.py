83e25c5fae360818c9c336064a16cbeca33d65c4
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-06 20:25:11 +0200

    feat: check the number of lines and print only those that are specified

--- lab_1.py
+++ lab_1.py
@@ -10,3 +10,8 @@ with open("pantadeus.txt", "r") as file:
 with open("pantadeus.txt", "r") as file:
     content = file.read().strip().split()
     print(content)
+
+with open("pantadeus.txt", "r") as file:
+    for line_number, line in enumerate(file, start=1):
+        if line_number in [1, 8,  12, 60, 98, 104]:
+            print(f"Line {line_number}: {line.strip()}")
