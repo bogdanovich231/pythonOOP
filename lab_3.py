449eaeb9c3e57e7e9b75c35b6f847bb85717512a
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-06 18:06:28 +0200

    feat: creating class Triangle| perimeter and footage methods

--- lab_3.py
+++ lab_3.py
@@ -33,6 +33,20 @@ class Rectangle(Figure):
     def calculate_perimeter(self):
         return 2 * (self.length + self.width)
 
+
+class Triangle(Figure):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def calculate_area(self):
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
+
+    def calculate_perimeter(self):
+        return self.side1 + self.side2 + self.side3
+
 choice = int(input("Choose a figure:\n1 - Square\n2 - Rectangle\n3 - Triangle\n4 - Circle\n5 - Rhombus\n6 - Trapeze\n"))
 
 if choice == 1:
@@ -47,3 +61,12 @@ elif choice == 2:
     rectangle = Rectangle(length, width)
     print("Rectangle footage:", rectangle.calculate_area())
     print("Perimeter of the rectangle:", rectangle.calculate_perimeter())
+
+elif choice == 3:
+    side1 = float(input("Enter the length of the first side of the triangle: "))
+    side2 = float(input("Enter the length of the second side of the triangle: "))
+    side3 = float(input("Enter the length of the third side of the triangle: "))
+    triangle = Triangle(side1, side2, side3)
+    print("Triangle footage:", triangle.calculate_area())
+    print("Triangle perimeter:", triangle.calculate_perimeter())
+
