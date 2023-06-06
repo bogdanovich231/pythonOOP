7b9ec3f31a63285feaca7c38981a662504079a3f
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-06 19:55:40 +0200

    feat: read and output text from a file

--- lab_1.py
+++ lab_1.py
@@ -1,3 +1,7 @@
 text = "Mr. Tadeusz, today is our last day!"
 with open("pantadeus.txt", "w") as file:
     file.write(text)
+
+with open("pantadeus.txt", "r") as file:
+    content = file.read()
+    print(content)
